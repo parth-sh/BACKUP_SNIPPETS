0a3221085a5798abb744a6352d1c737167541ee2
Parth Sharma <parthsharma8421@gmail.com>
2024-03-30 13:34:32 +0530

    update

--- DSA/Play.py
+++ DSA/Play.py
@@ -1,23 +1,16 @@
-def find_peak_element(nums):
-    n = len(nums)
-    
-    if n == 1:
-        return 0
-    if nums[0] > nums[1]:
-        return 0
-    if nums[n-2] < nums[n-1]:
-        return n-1
-    
-    lo, hi = 1, n-2
-    while lo <= hi:
-        mid = (lo + hi) // 2
-        if nums[mid - 1] < nums[mid] and nums[mid] > nums[mid + 1]:
-            return mid
-        elif nums[mid] < nums[mid + 1]:
-            lo = mid + 1
-        else:  # nums[mid - 1] > nums[mid]
-            hi = mid - 1
-    return -1
+class Stack:
+    def __init__(self):
+        self.arr = []
+        self.size = 0
 
-heights = list(map(int, input().split()))
-print(find_peak_element(heights))
+    def push(self):
+        pass
+    
+    def pop(self):
+        pass
+
+    def peek(self):
+        pass
+
+    def size(self):
+        pass
