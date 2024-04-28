df8eca4a3c4abb45999720650742e4d248f7cb1c
Parth Sharma <parthsharma8421@gmail.com>
2024-04-28 14:48:31 +0530

    update

--- DSA/BinaryTree.cpp
+++ DSA/BinaryTree.cpp
@@ -1,29 +1,36 @@
 #include "bits-stdc++.h"
 using namespace std;
 
-struct Node
-{
+struct Node {
     struct Node *left, *right;
     int data;
-    Node(int val)
-    {
+    Node(int val) {
         data = val;
         left = NULL;
         right = NULL;
     }
 };
 
-class BinaryTree
-{
+class BinaryTree {
     // There are 2 type of traversal techniques BFS/DFS
-    // Types of DFS techniques: InOrder Traversal, PreOrder Traversal, PostOrder Traversal
-    // Types of BFS: Level Order Traversal
+    // Types of DFS techniques: InOrder Traversal, PreOrder Traversal, PostOrder
+    // Traversal Types of BFS: Level Order Traversal, Radial Traversal (Radially
+    // upward downwards always)
 
-public:
-    void preorder(Node *root) // TC: O(N), SC: O(logN)
+   private:
+    int findIndex(vector<int> &inorder, int root) {
+        for (int i = 0; i < inorder.size(); i++) {
+            if (inorder[i] == root) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+   public:
+    void preorder(Node *root)  // TC: O(N), SC: O(logN)
     {
-        if (root == NULL)
-        {
+        if (root == NULL) {
             return;
         }
         cout << root->data << " ";
@@ -31,10 +38,9 @@ public:
         preorder(root->right);
     }
 
-    void inorder(Node *root) // TC: O(N), SC: O(logN)
+    void inorder(Node *root)  // TC: O(N), SC: O(logN)
     {
-        if (root == NULL)
-        {
+        if (root == NULL) {
             return;
         }
         inorder(root->left);
@@ -42,10 +48,9 @@ public:
         inorder(root->right);
     }
 
-    void postorder(Node *root) // TC: O(N), SC: O(logN)
+    void postorder(Node *root)  // TC: O(N), SC: O(logN)
     {
-        if (root == NULL)
-        {
+        if (root == NULL) {
             return;
         }
         postorder(root->left);
@@ -53,28 +58,22 @@ public:
         cout << root->data << " ";
     }
 
-    void levelorder(Node *root)
-    {
-        if (root == NULL)
-        {
+    void levelorder(Node *root) {
+        if (root == NULL) {
             return;
         }
         queue<Node *> q;
         q.push(root);
-        while (!q.empty())
-        {
+        while (!q.empty()) {
             int n = q.size();
-            for (int i = 0; i < n; i++)
-            {
+            for (int i = 0; i < n; i++) {
                 Node *temp = q.front();
                 q.pop();
                 cout << temp->data << " ";
-                if (temp->left)
-                {
+                if (temp->left) {
                     q.push(temp->left);
                 }
-                if (temp->right)
-                {
+                if (temp->right) {
                     q.push(temp->right);
                 }
             }
@@ -82,10 +81,10 @@ public:
     }
 
     // Level Order to Binary Tree
-    Node *insertLevelOrder(vector<int> &arr, int i, int n) // TC: O(N), SC: O(n) + O(logN)
+    Node *insertLevelOrder(vector<int> &arr, int i,
+                           int n)  // TC: O(N), SC: O(n) + O(logN)
     {
-        if (i < n)
-        {
+        if (i < n) {
             Node *root = new Node(arr[i]);
 
             root->left = insertLevelOrder(arr, 2 * i + 1, n);
@@ -96,64 +95,74 @@ public:
     }
 
     // All order traversal Preorder, Inorder, Postorder
-    void allordertraversal(Node *root)
-    {
+    void allordertraversal(Node *root) {
         stack<pair<Node *, int>> st;
         vector<vector<int>> ans(3);
-        if (root == NULL)
-        {
+        if (root == NULL) {
             return;
         }
         st.push({root, 1});
-        while (!st.empty())
-        {
+        while (!st.empty()) {
             pair<Node *, int> it = st.top();
             Node *temp = it.first;
             int touches = it.second;
             st.pop();
 
-            if (touches == 1)
-            {
+            if (touches == 1) {
                 ans[0].push_back(temp->data);
                 touches++;
                 st.push({temp, touches});
-                if (temp->left)
-                {
+                if (temp->left) {
                     st.push({temp->left, 1});
                 }
-            }
-            else if (touches == 2)
-            {
+            } else if (touches == 2) {
                 ans[1].push_back(temp->data);
                 touches++;
                 st.push({temp, touches});
-                if (temp->right)
-                {
+                if (temp->right) {
                     st.push({temp->right, 1});
                 }
-            }
-            else
-            {
+            } else {
                 ans[2].push_back(temp->data);
             }
         }
-        for (auto x : ans)
-        {
-            for (auto i : x)
-            {
+        for (auto x : ans) {
+            for (auto i : x) {
                 cout << i << " - ";
             }
             cout << endl;
         }
     }
+
+    // 105. Construct Binary Tree from Preorder and Inorder Traversal
+    Node *binaryTreeFromPreorderAndInorder(vector<int> &preorder,
+                                           vector<int> &inorder) {
+        if (preorder.size() == 0 || inorder.size() == 0) {
+            return NULL;
+        }
+        int rootIndexInInorder = findIndex(inorder, preorder[0]);
+        Node *root = new Node(preorder[0]);
+        vector<int> leftinorder(inorder.begin(),
+                                inorder.begin() + rootIndexInInorder);
+        vector<int> leftpreorder(preorder.begin() + 1,
+                                 preorder.begin() + rootIndexInInorder + 1);
+        root->left =
+            binaryTreeFromPreorderAndInorder(leftpreorder, leftinorder);
+        vector<int> rightinorder(inorder.begin() + rootIndexInInorder + 1,
+                                 inorder.end());
+        vector<int> rightpreorder(preorder.begin() + rootIndexInInorder + 1,
+                                  preorder.end());
+        root->right =
+            binaryTreeFromPreorderAndInorder(rightpreorder, rightinorder);
+        return root;
+    }
 };
 
-int main()
-{
-    Node *root = new Node(1);
-    root->left = new Node(2);
-    root->right = new Node(3);
-    root->left->right = new Node(5);
+int main() {
+    // Node *root = new Node(1);
+    // root->left = new Node(2);
+    // root->right = new Node(3);
+    // root->left->right = new Node(5);
 
     BinaryTree bt;
     // bt.preorder(root);
@@ -170,7 +179,13 @@ int main()
     // bt.levelorder(root2);
     // cout << endl;
 
-    bt.allordertraversal(root);
+    // bt.allordertraversal(root);
+
+    vector<int> preorder = {3, 9, 20, 15, 7};
+    vector<int> inorder = {9, 3, 15, 20, 7};
+    Node *root = bt.binaryTreeFromPreorderAndInorder(preorder, inorder);
+    bt.levelorder(root);
+    cout << endl;
 
     return 0;
 }
--- DSA/MySorting.cpp
+++ DSA/MySorting.cpp
@@ -1,56 +1,42 @@
 #include "bits-stdc++.h"
 using namespace std;
 
-class MySorting
-{
-private:
-    void merge(vector<int> &arr, int low, int mid, int high)
-    {
+class MySorting {
+   private:
+    void merge(vector<int> &arr, int low, int mid, int high) {
         const int n = high - low + 1;
         int *temp = new int[n];
         int pt1 = low, pt2 = mid + 1, j = 0;
-        while (pt1 <= mid && pt2 <= high && j < n)
-        {
-            if (arr[pt1] < arr[pt2])
-            {
+        while (pt1 <= mid && pt2 <= high && j < n) {
+            if (arr[pt1] < arr[pt2]) {
                 temp[j++] = arr[pt1++];
-            }
-            else
-            {
+            } else {
                 temp[j++] = arr[pt2++];
             }
         }
-        while (pt1 <= mid && j < n)
-        {
+        while (pt1 <= mid && j < n) {
             temp[j++] = arr[pt1++];
         }
-        while (pt2 <= high && j < n)
-        {
+        while (pt2 <= high && j < n) {
             temp[j++] = arr[pt2++];
         }
         j = 0;
-        for (int i = low; i <= high; i++)
-        {
+        for (int i = low; i <= high; i++) {
             arr[i] = temp[j++];
         }
     }
 
-    int partition(vector<int> &arr, int low, int high)
-    {
+    int partition(vector<int> &arr, int low, int high) {
         int pivotEle = arr[low];
         int start = low, end = high;
-        while (start < end)
-        {
-            while (start <= high - 1 && arr[start] <= pivotEle)
-            {
+        while (start < end) {
+            while (start <= high - 1 && arr[start] <= pivotEle) {
                 start++;
             }
-            while (end >= low + 1 && arr[end] > pivotEle)
-            {
+            while (end >= low + 1 && arr[end] > pivotEle) {
                 end--;
             }
-            if (start < end)
-            {
+            if (start < end) {
                 swap(arr[start], arr[end]);
             }
         }
@@ -58,81 +44,62 @@ private:
         return end;
     }
 
-public:
-    void selectionSort(vector<int> &arr)
-    {
+   public:
+    void selectionSort(vector<int> &arr) {
         int n = arr.size();
-        for (int i = 0; i < n; i++)
-        {
+        for (int i = 0; i < n; i++) {
             int min_idx = i;
-            for (int j = i + 1; j < n; j++)
-            {
-                if (arr[j] < arr[min_idx])
-                {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min_idx]) {
                     min_idx = j;
                 }
             }
-            if (min_idx != i)
-            {
+            if (min_idx != i) {
                 swap(arr[i], arr[min_idx]);
             }
         }
     }
 
-    void bubbleSort(vector<int> &arr)
-    {
+    void bubbleSort(vector<int> &arr) {
         int n = arr.size();
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n - i - 1; j++)
-            {
-                if (arr[j] > arr[j + 1])
-                {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
                     swap(arr[j], arr[j + 1]);
                 }
             }
         }
     }
 
-    void optimizedBubbleSort(vector<int> &arr)
-    {
+    void optimizedBubbleSort(vector<int> &arr) {
         int n = arr.size();
-        for (int i = 0; i < n; i++)
-        {
+        for (int i = 0; i < n; i++) {
             bool check = true;
-            for (int j = 0; j < n - i - 1; j++)
-            {
-                if (arr[j] > arr[j + 1])
-                {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
                     check = false;
                     swap(arr[j], arr[j + 1]);
                 }
             }
-            if (check)
-            {
+            if (check) {
                 break;
             }
         }
     }
 
-    void insertionSort(vector<int> &arr)
-    {
+    void insertionSort(vector<int> &arr) {
         int n = arr.size();
-        for (int i = 0; i < n; i++)
-        {
+        for (int i = 0; i < n; i++) {
             int j = i;
-            while (j > 0 && arr[j] < arr[j - 1])
-            {
+            while (j > 0 && arr[j] < arr[j - 1]) {
                 swap(arr[j], arr[j - 1]);
                 j--;
             }
         }
     }
 
-    void mergeSort(vector<int> &arr, int low, int high)
-    {
-        if (low >= high)
-        {
+    void mergeSort(vector<int> &arr, int low, int high) {
+        if (low >= high) {
             return;
         }
         int mid = (low + high) / 2;
@@ -141,10 +108,8 @@ public:
         merge(arr, low, mid, high);
     }
 
-    void quickSort(vector<int> &nums, int low, int high)
-    {
-        if (low >= high)
-        {
+    void quickSort(vector<int> &nums, int low, int high) {
+        if (low >= high) {
             return;
         }
         int pivot = partition(nums, low, high);
@@ -153,17 +118,14 @@ public:
     }
 };
 
-void printArr(vector<int> &nums)
-{
+void printArr(vector<int> &nums) {
     int n = nums.size();
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
         cout << nums[i] << " - ";
     }
 }
 
-int main()
-{
+int main() {
     MySorting sort;
 
     vector<int> nums = {5, 4, 3, 2, 1};
