1b5619ef814b1bb084cf1203266f160919a5d587
Parth Sharma <parthsharma8421@gmail.com>
2024-05-25 02:25:56 +0530

    update

---
+++ DSA/Backtracking.cpp
@@ -0,0 +1,14 @@
+// 1863. Sum of All Subset XOR Totals
+// https://leetcode.com/problems/sum-of-all-subset-xor-totals/submissions/1263230933/?envType=daily-question&envId=2024-05-20
+
+// 78. Subsets
+// https://leetcode.com/problems/subsets/submissions/1264183656/?envType=daily-question&envId=2024-05-21
+
+// 131. Palindrome Partitioning
+// https://leetcode.com/problems/palindrome-partitioning/submissions/1265221696/?envType=daily-question&envId=2024-05-22
+
+// 2597. The Number of Beautiful Subsets
+// https://leetcode.com/problems/the-number-of-beautiful-subsets/solutions/5195346/only-backtracking-concise-newbie-friendly-3-lang-simple-explanation/?envType=daily-question&envId=2024-05-23
+
+// 1255. Maximum Score Words Formed by Letters
+// https://leetcode.com/problems/maximum-score-words-formed-by-letters/submissions/1266931183/?envType=daily-question&envId=2024-05-24
