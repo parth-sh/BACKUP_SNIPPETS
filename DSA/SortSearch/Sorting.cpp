56a31aac716edc030dea0a7df3cf7f6858fede2c
Parth Sharma <parthsharma8421@gmail.com>
2024-06-05 20:37:09 +0530

    update

--- DSA/SortSearch/Sorting.cpp
+++ DSA/SortSearch/Sorting.cpp
@@ -104,6 +104,9 @@ class MySorting {
         mergeSort(arr, low, mid);
         mergeSort(arr, mid + 1, high);
         merge(arr, low, mid, high);
+
+        // 88. Merge Sorted Aray
+        // https://leetcode.com/problems/merge-sorted-array/submissions/1274040708/
     }
 
     void quickSort(vector<int> &nums, int start, int end) {
@@ -125,19 +128,24 @@ class MySorting {
                 swap(nums[i], nums[nums[i] - 1]);
             }
         }
-    }
 
-    // 268. Missing Number
-    // https://leetcode.com/problems/missing-number/submissions/1257064517/
-    // 448. Find All Numbers Disappeared in an Array
-    // https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/submissions/1257074682/
-    // 287. Find the Duplicate Number
-    // https://leetcode.com/problems/find-the-duplicate-number/submissions/1257230928/
-    // 442. Find All Duplicates in an Array
-    // https://leetcode.com/problems/find-all-duplicates-in-an-array/submissions/1257234180/
-    // 645. Set Mismatch
-    // 41. First Missing Positive
-    // https://leetcode.com/problems/first-missing-positive/submissions/1257257377/
+        // 268. Missing Number
+        // https://leetcode.com/problems/missing-number/submissions/1257064517/
+
+        // 448. Find All Numbers Disappeared in an Array
+        // https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/submissions/1257074682/
+
+        // 287. Find the Duplicate Number
+        // https://leetcode.com/problems/find-the-duplicate-number/submissions/1257230928/
+
+        // 442. Find All Duplicates in an Array
+        // https://leetcode.com/problems/find-all-duplicates-in-an-array/submissions/1257234180/
+
+        // 645. Set Mismatch
+
+        // 41. First Missing Positive
+        // https://leetcode.com/problems/first-missing-positive/submissions/1257257377/
+    }
 };
 
 void printArr(vector<int> &nums) {
